c975d81681cfc677619e47fe64094b4c5ace26e7
michel heon <heon@ubuntu>
2020-11-30 05:22:15 -0500

    Restructuration des pom-files pour l'intégration de 'fr' dans Vitro-languages

---
+++ script/clone_vivo_all.bash
@@ -0,0 +1,15 @@
+#!bin/bash
+BRANCHE=ehess-dev
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/Vitro.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/Vitro-fr_FR.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/Vitro-EHESS.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/Vitro-languages.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/Vitro-languages_fr_FR.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/Vitro-languages-EHESS.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/VIVO.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/VIVO-fr_FR.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/VIVO-EHESS.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/VIVO-languages.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/VIVO-languages-fr_FR.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/VIVO-languages-EHESS.git
+git clone --single-branch --branch ${BRANCHE} https://github.com/dsi-ehess/VIVO-Sample-Data.git
